9d8abd0f8ae03ff2404fa0fdfd5357d23fe89f64
mohamedADAM27 <adam1498y@gmail.com>
2026-01-18 15:25:00 +0530

    Add files via upload

---
+++ rotatearrays.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+
+/*class rotatearrays{
+    public static void main(String[] args) {
+        int[] nums = {992,3,4,5,6,7};
+        int k=3;
+
+        int n = nums.length;
+        k=k%n;
+          rotatearrays obj = new rotatearrays();
+
+        obj.rotate(nums, 0, n - 1);
+        obj.rotate(nums, 0, k - 1);
+        obj.rotate(nums, k, n - 1);
+
+        
+        System.out.println(Arrays.toString(nums));
+    }
+        void rotate(int[] nums, int start,int end){
+            while(end>start){
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+
+            start++;
+            end--;
+        }
+    }
+}*/
